247d3b8c995e258c60a19fc100d5743590bacb97
Kamakshi Karakoti <kamakshikarakoti22@gmail.com>
2023-03-06 09:30:42 +0530

    deploy

--- android/app/src/main/kotlin/com/example/task_list_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.task_list_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color warningColor = Color.fromRGBO(245, 93, 92,1);
-const Color successColor = Color.fromRGBO(237, 125, 41,1);
-const Color primaryColor = Color.fromRGBO(221, 221, 231, 1);
-const Color secondaryColor = Color.fromRGBO(146, 139, 133,1);
-const Color backgroundColor = Color.fromRGBO(39, 41, 56,1);
